16f43ec6a39db293b9b3db2377069cd63d8dc731
eldskald <rafaelbordoni00@gmail.com>
2022-05-07 02:36:35 -0300

    Make stories as the project wanted it to be

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,32 +1,60 @@
 
-function Story (props) {
-    return (
-        <div class="story">
-            <div class="icone">
-                <img src="images/stories_background.jpg" />
-                <img src={props.picture} />
-            </div>
-            <h1>{props.name}</h1>
-        </div>
-    );
-}
+const stories = [
+    {
+        name: "9gag",
+        picture: "images/9gag.png"
+    },
+    {
+        name: "meowed",
+        picture: "images/meowed.png"
+    },
+    {
+        name: "barked",
+        picture: "images/barked.png"
+    },
+    {
+        name: "nathanpyle...",
+        picture: "images/nathanwpylestrangeplanet.png"
+    },
+    {
+        name: "wawawiwac...",
+        picture: "images/wawawiwacomicsa.png"
+    },
+    {
+        name: "respondeai",
+        picture: "images/respondeai.png"
+    },
+    {
+        name: "filomoderna",
+        picture: "images/filomoderna.png"
+    },
+    {
+        name: "memeariago...",
+        picture: "images/memeariagourmet.png"
+    }
+]
 
 export default function Stories () {
+    function Story (props) {
+        return (
+            <div class="story">
+                <div class="icone">
+                    <img src="images/stories_background.jpg" />
+                    <img src={props.picture} />
+                </div>
+                <h1>{props.name}</h1>
+            </div>
+        );
+    }
+
     return (
         <div class="stories-container">
+            
+            {stories.map((story) => <Story name={story.name} picture={story.picture} />)}
+
             <div class="botao-scroll">
                 <ion-icon name="chevron-forward-circle"></ion-icon>
             </div>
-
-            <Story name="9gag" picture="images/9gag.png" />
-            <Story name="meowed" picture="images/meowed.png" />
-            <Story name="barked" picture="images/barked.png" />
-            <Story name="nathanpyle..." picture="images/nathanwpylestrangeplanet.png" />
-            <Story name="wawawiwac..." picture="images/wawawiwacomicsa.png" />
-            <Story name="respondeai" picture="images/respondeai.png" />
-            <Story name="filomoderna" picture="images/filomoderna.png" />
-            <Story name="memeariago..." picture="images/memeriagourmet.png" />
-
         </div>
     );
 }
